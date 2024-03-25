763946c37629c1a8916efb999dc9f9e773234ce1
bbungjin <leejintwo12@naver.com>
2024-03-25 09:16:39 +0900

    feat: 초기설정

---
+++ src/main/java/kuslide/team/kuslide/KuslideApplication.java
@@ -0,0 +1,13 @@
+package kuslide.team.kuslide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuslideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuslideApplication.class, args);
+	}
+
+}
